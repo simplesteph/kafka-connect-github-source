812875b55d9d5a838199429f7a834163b021159f
Stephane Maarek <stephane.maarek@gmail.com>
2017-04-28 21:17:31 +1000

    better run.sh

--- run.sh
+++ run.sh
@@ -1,3 +1,23 @@
 #!/usr/bin/env bash
 export CLASSPATH="$(find target/ -type f -name '*.jar'| grep '\-package' | tr '\n' ':')"
-connect-standalone config/worker.properties config/GitHubSourceConnectorExample.properties
+if hash docker 2>/dev/null; then
+    # for docker lovers
+    docker run -e CLASSPATH=$CLASSPATH \
+           --net=host --rm \
+           -v $(pwd):/kafka-connect-github-source \
+           -w /kafka-connect-github-source \
+           confluentinc/cp-kafka-connect:3.2.0 \
+           connect-standalone config/worker.properties config/GitHubSourceConnectorExample.properties
+elif hash connect-standalone 2>/dev/null; then
+    # for mac users who used homebrew
+    connect-standalone config/worker.properties config/GitHubSourceConnectorExample.properties
+elif [[ -z $KAFKA_HOME ]]; then
+    # for people who installed kafka vanilla
+    $KAFKA_HOME/bin/connect-standalone.sh $KAFKA_HOME/etc/schema-registry/connect-avro-standalone.properties config/MySourceConnector.properties
+elif [[ -z $CONFLUENT_HOME ]]; then
+    # for people who installed kafka confluent flavour
+    $CONFLUENT_HOME/bin/connect-standalone $CONFLUENT_HOME/etc/schema-registry/connect-avro-standalone.properties config/MySourceConnector.properties
+else
+    printf "Couldn't find a suitable way to run kafka connect for you.\n \
+Please install Docker, or download the kafka binaries and set the variable KAFKA_HOME."
+fi;
