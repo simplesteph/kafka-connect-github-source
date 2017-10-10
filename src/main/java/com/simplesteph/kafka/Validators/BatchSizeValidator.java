5b77faac53017e6c7e7148c29d303bf2566dfe88
itERRatOR <itERRatOR@users.noreply.github.com>
2017-10-10 23:22:50 +1100

    Code format in BatchSizeValidator

--- src/main/java/com/simplesteph/kafka/Validators/BatchSizeValidator.java
+++ src/main/java/com/simplesteph/kafka/Validators/BatchSizeValidator.java
@@ -8,7 +8,7 @@ public class BatchSizeValidator implements ConfigDef.Validator {
     @Override
     public void ensureValid(String name, Object value) {
         Integer batchSize = (Integer) value;
-        if (!(1 <= batchSize && batchSize <=100)){
+        if (!(1 <= batchSize && batchSize <= 100)){
             throw new ConfigException(name, value, "Batch Size must be a positive integer that's less or equal to 100");
         }
     }
