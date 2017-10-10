754f231fe54a28e36a20b608181540d48acf5c96
itERRatOR <zambi1985@mail.ru>
2017-10-11 00:41:39 +1100

    Add `final` modifier for constants

--- src/main/java/com/simplesteph/kafka/GitHubSchemas.java
+++ src/main/java/com/simplesteph/kafka/GitHubSchemas.java
@@ -6,39 +6,39 @@ import org.apache.kafka.connect.data.Timestamp;
 
 public class GitHubSchemas {
 
-    public static String NEXT_PAGE_FIELD = "next_page";
+    public static final String NEXT_PAGE_FIELD = "next_page";
 
     // Issue fields
-    public static String OWNER_FIELD = "owner";
-    public static String REPOSITORY_FIELD = "repository";
-    public static String CREATED_AT_FIELD = "created_at";
-    public static String UPDATED_AT_FIELD = "updated_at";
-    public static String NUMBER_FIELD = "number";
-    public static String URL_FIELD = "url";
-    public static String HTML_URL_FIELD = "html_url";
-    public static String TITLE_FIELD = "title";
-    public static String STATE_FIELD = "state";
+    public static final String OWNER_FIELD = "owner";
+    public static final String REPOSITORY_FIELD = "repository";
+    public static final String CREATED_AT_FIELD = "created_at";
+    public static final String UPDATED_AT_FIELD = "updated_at";
+    public static final String NUMBER_FIELD = "number";
+    public static final String URL_FIELD = "url";
+    public static final String HTML_URL_FIELD = "html_url";
+    public static final String TITLE_FIELD = "title";
+    public static final String STATE_FIELD = "state";
 
     // User fields
-    public static String USER_FIELD = "user";
-    public static String USER_URL_FIELD = "url";
-    public static String USER_HTML_URL_FIELD = "html_url";
-    public static String USER_ID_FIELD = "id";
-    public static String USER_LOGIN_FIELD = "login";
+    public static final String USER_FIELD = "user";
+    public static final String USER_URL_FIELD = "url";
+    public static final String USER_HTML_URL_FIELD = "html_url";
+    public static final String USER_ID_FIELD = "id";
+    public static final String USER_LOGIN_FIELD = "login";
 
     // PR fields
-    public static String PR_FIELD = "pull_request";
-    public static String PR_URL_FIELD = "url";
-    public static String PR_HTML_URL_FIELD = "html_url";
+    public static final String PR_FIELD = "pull_request";
+    public static final String PR_URL_FIELD = "url";
+    public static final String PR_HTML_URL_FIELD = "html_url";
 
     // Schema names
-    public static String SCHEMA_KEY = "com.simplesteph.kafka.connect.github.IssueKey";
-    public static String SCHEMA_VALUE_ISSUE = "com.simplesteph.kafka.connect.github.IssueValue";
-    public static String SCHEMA_VALUE_USER = "com.simplesteph.kafka.connect.github.UserValue";
-    public static String SCHEMA_VALUE_PR = "com.simplesteph.kafka.connect.github.PrValue";
+    public static final String SCHEMA_KEY = "com.simplesteph.kafka.connect.github.IssueKey";
+    public static final String SCHEMA_VALUE_ISSUE = "com.simplesteph.kafka.connect.github.IssueValue";
+    public static final String SCHEMA_VALUE_USER = "com.simplesteph.kafka.connect.github.UserValue";
+    public static final String SCHEMA_VALUE_PR = "com.simplesteph.kafka.connect.github.PrValue";
 
     // Key Schema
-    public static Schema KEY_SCHEMA = SchemaBuilder.struct().name(SCHEMA_KEY)
+    public static final Schema KEY_SCHEMA = SchemaBuilder.struct().name(SCHEMA_KEY)
             .version(1)
             .field(OWNER_FIELD, Schema.STRING_SCHEMA)
             .field(REPOSITORY_FIELD, Schema.STRING_SCHEMA)
@@ -46,7 +46,7 @@ public class GitHubSchemas {
             .build();
 
     // Value Schema
-    public static Schema USER_SCHEMA = SchemaBuilder.struct().name(SCHEMA_VALUE_USER)
+    public static final Schema USER_SCHEMA = SchemaBuilder.struct().name(SCHEMA_VALUE_USER)
             .version(1)
             .field(USER_URL_FIELD, Schema.STRING_SCHEMA)
             .field(USER_ID_FIELD, Schema.INT32_SCHEMA)
@@ -54,14 +54,14 @@ public class GitHubSchemas {
             .build();
 
     // optional schema
-    public static Schema PR_SCHEMA = SchemaBuilder.struct().name(SCHEMA_VALUE_PR)
+    public static final Schema PR_SCHEMA = SchemaBuilder.struct().name(SCHEMA_VALUE_PR)
             .version(1)
             .field(PR_URL_FIELD, Schema.STRING_SCHEMA)
             .field(PR_HTML_URL_FIELD, Schema.STRING_SCHEMA)
             .optional()
             .build();
 
-    public static Schema VALUE_SCHEMA = SchemaBuilder.struct().name(SCHEMA_VALUE_ISSUE)
+    public static final Schema VALUE_SCHEMA = SchemaBuilder.struct().name(SCHEMA_VALUE_ISSUE)
             .version(2)
             .field(URL_FIELD, Schema.STRING_SCHEMA)
             .field(TITLE_FIELD, Schema.STRING_SCHEMA)
